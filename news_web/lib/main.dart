1bfd7e8237e95171591f263a78e73eb240fbf453
mahsak01 <mahsakarimi01@yahoo.com>
2021-08-22 19:55:27 +0430

    add app bar and background of home page web

---
+++ news_web/lib/main.dart
@@ -0,0 +1,23 @@
+//
+import 'package:flutter/material.dart';
+
+import 'screens/HomePage.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Color(0xff707070),
+          accentColor: Color(0xffcc2626),
+          backgroundColor: Colors.white
+      ),
+      title: 'NewsApp',
+      home: HomePage(),
+    );
+  }
+}
---
+++ news_web/lib/screens/HomePage.dart
@@ -0,0 +1,121 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+
+///home page
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      ///app bar
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.centerRight,
+              children: [
+                Stack(
+                  alignment: Alignment.centerLeft,
+                  children: [
+                    Container(
+                      color: Theme.of(context).backgroundColor,
+                      width: MediaQuery.of(context).size.width,
+                      height: 100,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          color: Theme.of(context).accentColor,
+                          height: 100,
+                          width: MediaQuery.of(context).size.width/12,
+                        ),
+                        Container(
+                          color: Theme.of(context).primaryColor,
+                          height: 100,
+                          width:  MediaQuery.of(context).size.width/12,
+                        ),
+                        Container(
+                          color: Theme.of(context).accentColor,
+                          height: 100,
+                          width:  MediaQuery.of(context).size.width/12,
+                        ),
+                        Container(
+                          color: Theme.of(context).primaryColor,
+                          height: 100,
+                          width:  MediaQuery.of(context).size.width/12,
+                        ),
+
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      color: Theme.of(context).primaryColor,
+                      height: 100,
+                      width:  MediaQuery.of(context).size.width/12,
+                    ),
+                    Container(
+                      color: Theme.of(context).accentColor,
+                      height: 100,
+                      width:  MediaQuery.of(context).size.width/12,
+                    ),
+                    Container(
+                      color: Theme.of(context).primaryColor,
+                      height: 100,
+                      width:  MediaQuery.of(context).size.width/12,
+                    ),
+                    Container(
+                      color: Theme.of(context).accentColor,
+                      height: 100,
+                      width:  MediaQuery.of(context).size.width/12,
+                    ),
+
+
+                  ],
+                )
+              ],
+            ),
+            ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 20, sigmaY:1),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 100,
+                  color: Theme.of(context).primaryColor.withOpacity(0.5),
+
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: SvgPicture.asset('assets/icon.svg'),
+            )
+          ],
+        )
+      ),
+
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          children: [
+            Container(
+              color: Theme.of(context).accentColor,
+              width:  MediaQuery.of(context).size.width/12,
+            )
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
